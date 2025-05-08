49c469125e6a9d8e5ca84cfc01d0d7f06c4f210c
spawner <spawner6@outlook.com>
2025-05-08 14:33:57 +0800

    Update smea.py

--- scripts/smea.py
+++ scripts/smea.py
@@ -16,18 +16,18 @@ try:
             def __init__(self):
                 if not smea.INITIALIZED:
                     samplers_smea = [
-                        ("Spawner SMEA", sample_spawner_smea, ["k_spawner_smea"], {}),
-                        ("Spawner SMEA (beta)", sample_spawner_smea_beta, ["k_spawner_smea_beta"], {}),
-                        ("Spawner SMEA Dyn (beta)", sample_spawner_smea_dyn_beta, ["k_spawner_smea_dyn_beta"], {}),
+                        ("Spawner SMEA", sample_spawner_smea, ["k_spawner_smea"], {'scheduler': 'karras', "uses_ensd": True}),
+                        ("Spawner SMEA (beta)", sample_spawner_smea_beta, ["k_spawner_smea_beta"], {'scheduler': 'karras', "uses_ensd": True}),
+                        ("Spawner SMEA Dyn (beta)", sample_spawner_smea_dyn_beta, ["k_spawner_smea_dyn_beta"], {'scheduler': 'karras',"uses_ensd": True}),
                     ]
                     samplers_data_smea = [
                         sd_samplers_common.SamplerData(label, lambda model, funcname=funcname: KDiffusionSampler(funcname, model), aliases, options)
                         for label, funcname, aliases, options in samplers_smea
                         if callable(funcname)
                     ]
-                    sampler_extra_params["sample_spawner_smea"] = ["s_churn", "s_tmin", "s_tmax", "s_noise"]
-                    sampler_extra_params["sample_spawner_smea_beta"] = ["s_churn", "s_tmin", "s_tmax", "s_noise"]
-                    sampler_extra_params["sample_spawner_smea_dyn_beta"] = ["s_churn", "s_tmin", "s_tmax", "s_noise"]
+                    sampler_extra_params["sample_spawner_smea"] = ['s_noise', 'eta']
+                    sampler_extra_params["sample_spawner_smea_beta"] = ['eta', 's_noise', 'beta']
+                    sampler_extra_params["sample_spawner_smea_dyn_beta"] = ['eta_start', 'eta_end', 'eta_exponent', 's_noise', 'beta', 'sigma_max_for_dyn_eta']
                     
                     sd_samplers.all_samplers.extend(samplers_data_smea)
                     sd_samplers.all_samplers_map = {x.name: x for x in sd_samplers.all_samplers}
