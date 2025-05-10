e49b7725fdee18c737401167815e7a2385c45f1e
spawner <spawner6@outlook.com>
2025-05-10 23:42:43 +0800

    Update smea.py

--- scripts/smea.py
+++ scripts/smea.py
@@ -1,6 +1,6 @@
 try:
     import smea
-    from smea import sample_spawner_smea, sample_spawner_smea_beta, sample_spawner_smea_dyn_beta
+    from smea import sample_spawner_smea, sample_spawner_smea_beta, sample_spawner_smea_dyn_beta, sample_spawner_smea_dyn_beta1
 
     if smea.BACKEND == "WebUI":
         from modules import scripts, sd_samplers_common, sd_samplers
@@ -19,6 +19,7 @@ try:
                         ("Spawner SMEA", sample_spawner_smea, ["k_spawner_smea"], {"uses_ensd": True}),
                         ("Spawner SMEA (beta)", sample_spawner_smea_beta, ["k_spawner_smea_beta"], {"uses_ensd": True}),
                         ("Spawner SMEA Dyn (beta)", sample_spawner_smea_dyn_beta, ["k_spawner_smea_dyn_beta"], {"uses_ensd": True}),
+                        ("Spawner SMEA Dyn (beta1)", sample_spawner_smea_dyn_beta1, ["k_spawner_smea_dyn_beta1"], {"uses_ensd": True}),
                     ]
                     samplers_data_smea = [
                         sd_samplers_common.SamplerData(label, lambda model, funcname=funcname: KDiffusionSampler(funcname, model), aliases, options)
@@ -28,6 +29,7 @@ try:
                     sampler_extra_params["sample_spawner_smea"] = ['s_noise', 'eta']
                     sampler_extra_params["sample_spawner_smea_beta"] = ['eta', 's_noise', 'beta']
                     sampler_extra_params["sample_spawner_smea_dyn_beta"] = ['eta_start', 'eta_end', 'eta_exponent', 's_noise', 'beta', 'sigma_max_for_dyn_eta']
+                    sampler_extra_params["sample_spawner_smea_dyn_beta1"] = ['eta_start', 'eta_end', 'eta_exponent', 's_noise', 'beta', 'sigma_max_for_dyn_eta']
                     
                     sd_samplers.all_samplers.extend(samplers_data_smea)
                     sd_samplers.all_samplers_map = {x.name: x for x in sd_samplers.all_samplers}
